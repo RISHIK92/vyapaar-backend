51335a69a04134194153187b36adc6a15444d13e
Rishik <rishik3555@gmail.com>
2025-05-15 01:41:37 +0530

    deploy

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://vyapaar-frontend.vercel.app/",
+    origin: "https://vyapaar-frontend.vercel.app",
     credentials: true,
   })
 );
